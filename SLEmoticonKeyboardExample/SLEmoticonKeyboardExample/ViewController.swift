9bc6157a1760f1cdda8e56ce09346ccc7d3cb020
CoderSLZeng <359297567@qq.com>
2017-03-17 17:56:51 +0800

    插入普通表情

--- SLEmoticonKeyboardExample/SLEmoticonKeyboardExample/ViewController.swift
+++ SLEmoticonKeyboardExample/SLEmoticonKeyboardExample/ViewController.swift
@@ -40,7 +40,33 @@ class ViewController: UIViewController {
         }
         
         // 4.普通表情:图文混排
+        // 4.1.根据图片路径创建属性字符串
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(contentsOfFile: emoticon.pngPath!)
+        let font = textView.font!
+        attachment.bounds = CGRect(x: 0, y: -4, width: font.lineHeight, height: font.lineHeight)
+        let attrImageStr = NSAttributedString(attachment: attachment)
+        
+        // 4.2.创建可变的属性字符串
+        let attrMStr = NSMutableAttributedString(attributedString: textView.attributedText)
+        
+        // 4.3.将图片属性字符串,替换到可变属性字符串的某一个位置
+        // 4.3.1.获取光标所在的位置
+        let range = textView.selectedRange
+        
+        // 4.3.2.替换属性字符串
+        attrMStr.replaceCharactersInRange(range, withAttributedString: attrImageStr)
+        
+        // 显示属性字符串
+        textView.attributedText = attrMStr
+        
+        // 将文字的大小重置
+        textView.font = font
+        
+        // 将光标设置回原来位置 + 1
+        textView.selectedRange = NSRange(location: range.location + 1, length: 0)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
